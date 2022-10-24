a4c15f50fbb6fa5f49e36f611774caed55e0093b
FRANKEE6 <frankee6@pokec.sk>
2022-10-24 16:16:37 +0200

    pridaný normalize.css a stylesheet.css

--- partials/header.php
+++ partials/header.php
@@ -6,12 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>to-do app</title>
+    <link rel="stylesheet" href="/todoapp/assets/node_modules/normalize.css/normalize.css">
     <link rel="stylesheet" href="/todoapp/assets/node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/todoapp/assets/css/stylesheet.css">
 </head>
 
 <body>
 
-    <header>
+    <header class="page-header">
         <h1>Moja to-do appka</h1>
     </header>
 
