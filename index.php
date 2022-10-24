4ed71c3019742fcb31603ed8cc309212540f19f8
FRANKEE6 <frankee6@pokec.sk>
2022-10-24 15:45:44 +0200

    Vytvoril som základnú štruktúru projektu

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include "./partials/header.php" ?>
+
+<div class="container">
+    <ul class="list-group col-sm-6">
+        <li class="list-goup-item">You really need to do this</li>
+        <li class="list-goup-item">But you also need to do this other thing</li>
+    </ul>
+</div>
+
+<?php include "./partials/footer.php" ?>
---
+++ partials/footer.php
@@ -0,0 +1,9 @@
+</main>
+
+<footer>
+
+</footer>
+<script src="../assets/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+
+</html>
