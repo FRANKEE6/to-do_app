98d3df7d7ac20cee9fa1ce151f58c5592fa86fd6
FRANKEE6 <frankee6@pokec.sk>
2022-10-24 20:21:22 +0200

    setup 4 javascript

--- _partials/footer.php
+++ _partials/footer.php
@@ -3,7 +3,9 @@
 <footer>
 
 </footer>
-<script src="/todoapp/assets/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+<script src="assets/js/app.js"></script>
 </body>
 
 </html>
