cdfdded9147eed508c27f69faca3fa2286c1675b
FRANKEE6 <frankee6@pokec.sk>
2022-10-24 17:54:58 +0200

    Vloženie caftan/medoo pre prácu s databázou + prepojenie v config.php

--- _inc/config.php
+++ _inc/config.php
@@ -1,1 +1,15 @@
-<p>i am here </p>
+<?php
+// Require Composer's autoloader.
+require 'assets/vendor/autoload.php';
+
+// Using Medoo namespace.
+use Medoo\Medoo;
+
+// Connect the database.
+$database = new Medoo([
+    'type' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'todo',
+    'username' => 'FRANKEE',
+    'password' => '8EE2pknk'
+]);
