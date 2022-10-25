1330457962a37c9d6d2197157232be41df27bdcd
FRANKEE6 <frankee6@pokec.sk>
2022-10-25 17:51:11 +0200

    pridaný back button v edit.php

--- edit.php
+++ edit.php
@@ -31,6 +31,9 @@ include_once "_partials/header.php";
                 <!-- Skrytý input vo formulári ktorý v sebe nesie ID ktoré sme dostali-->
                 <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
                 <input class="btn btn-sm btn-danger" type="submit" value="edit item">
+                <span>
+                    <a class="btn btn-sm btn-info" role="button" href="<?php echo $domovindex ?>">Go back</a>
+                </span>
             </p>
         </form>
     </div>
