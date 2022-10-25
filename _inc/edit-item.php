dd1217739c1e258e05bb136a7f95727e0d5f3114
FRANKEE6 <frankee6@pokec.sk>
2022-10-25 16:46:10 +0200

    uprava nazvov premennych

--- _inc/edit-item.php
+++ _inc/edit-item.php
@@ -2,7 +2,7 @@
 require_once 'config.php';
 
 // update item
-$id = $database->update(
+$affected = $database->update(
     'items',
     [
         'text' => $_POST['message']
@@ -14,7 +14,7 @@ $id = $database->update(
 );
 
 // Ak bolo ID úspešne nájdené, kód sa ukončí s hláškou 'success' ktorú odchytáva java-script
-if ($id) {
+if ($$affected) {
     header('Location:' . $domovindex);
     die('success');
 }
