6fbfd28d2714a7ef3c4bb16797e0b0396c95f834
FRANKEE6 <frankee6@pokec.sk>
2022-10-25 14:10:17 +0200

    dokončenie 404 stránky

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+include_once "_partials/header.php";
+?>
+
+<h2>404</h2>
+
+<div class="container">
+    <div class="row">
+        <p>
+            not found :(
+        </p>
+    </div>
+</div>
+
+
+<?php include_once "_partials/footer.php" ?>
