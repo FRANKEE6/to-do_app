ff3d2958b7200cb44661a1e3cf4d6b03b7d724d5
FRANKEE6 <frankee6@pokec.sk>
2022-10-25 18:03:43 +0200

    pridaný delete-item.php

---
+++ _inc/delete-item.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'config.php';
+
+// update item
+$affected = $database->update(
+    'items',
+    [
+        'text' => $_POST['message']
+    ],
+    [
+        'id' => $_POST['id']
+
+    ]
+);
+
+// Ak bolo ID úspešne nájdené, kód sa ukončí s hláškou 'success' ktorú odchytáva java-script
+if ($$affected) {
+    header('Location:' . $domovindex);
+    die('success');
+}
